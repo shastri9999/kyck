03ee92a3100a07fcc323a1f6e0002c02cc6182c2
shastri9999 <shastri9999@gmail.com>
2016-10-28 15:31:43 +0530

    1.Adding CSS reset 2. HTML5 mode URLs

--- src/app/index.routes.js
+++ src/app/index.routes.js
@@ -1,8 +1,9 @@
 'use strict';
 
-function routeConfig($urlRouterProvider, $stateProvider) {
+function routeConfig($urlRouterProvider, $stateProvider, $locationProvider) {
   'ngInject';
   $urlRouterProvider.otherwise('/');
+  $locationProvider.html5Mode(true);
 }
 
 export default angular
