ddc839f810f156f79db98dc4f1a15dbb74eeacaa
shastri9999 <shastri9999@gmail.com>
2016-10-28 16:53:05 +0530

    Fixing build mode

--- src/app/index.bootstrap.js
+++ src/app/index.bootstrap.js
@@ -1,8 +1,8 @@
 'use strict';
-
 import '!!file-loader?name=[name].[ext]!../favicon.ico';
 import "./index.module";
 import "../assets/styles/sass/index.scss";
+import "angular";
 
 angular.element(document).ready(function () {
 	angular.bootstrap(document, ['kyck'], {
