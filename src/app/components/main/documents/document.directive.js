3cb0c79c9e003dc724dca3986fbc5701308360be
Rajesh Veeranki <rajesh@swiggy.in>
2016-11-07 01:52:25 +0530

    Added file upload

--- src/app/components/main/documents/document.directive.js
+++ src/app/components/main/documents/document.directive.js
@@ -16,29 +16,45 @@ function documentComponent($log, $state, Upload, DocumentResource) {
 
   return directive;
 
-  function DocumentController ($scope) {
+  function DocumentController ($scope, $mdToast) {
     'ngInject';
 	  $log.debug('Hello from Document controller!');
-    $scope.upload = function(file, documentType){
+
+    $scope.upload = function(file, document){
         $log.debug(file);
         //$state.go('main.document.preview',{ picFile: file});
         
         Upload.upload({
-          url: '/kyck-rest/document/upload',
+          url: '/kyck-rest/document/upload?documentType='+document.documentType,
           data: {
-            multipartFile: file,
-            documentType: documentType
+            file: file
           }
         }).then(function(response){
             $log.debug(response);
-            $log.debug('Success: ' + response.config.data.file.name + 'Uploaded. Response: ' + resp.data);
+            $log.debug('Success: ' + response.config.data.file.name + 'Uploaded. Response: ' + response.data);
+            $mdToast.show(
+              $mdToast.simple()
+                .textContent('File Uploaded!')
+                .position('bottom right')
+                .toastClass('md-primary')
+                .hideDelay(2000)
+            );
         }, function(error){
             $log.debug(error);
+            $mdToast.show(
+              $mdToast.simple()
+                .textContent('File Upload Failed!')
+                .position('bottom right')
+                .toastClass('md-warn')
+                .hideDelay(2000)
+            );
+
         }, function(evt){
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
             $log.debug(progressPercentage);
             //console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
         });
+        
         
        //DocumentResource.upload({mul})
     }
