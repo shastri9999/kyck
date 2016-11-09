53c87566b5b8f587f9d7b117befa666998b00fec
shastri9999 <shastri9999@gmail.com>
2016-11-10 02:50:32 +0530

    Adding UI for blocks

--- src/app/components/main/blockchain/blockchain.controller.js
+++ src/app/components/main/blockchain/blockchain.controller.js
@@ -1,8 +1,36 @@
 'use strict';
 
 
-function BlockChainController() {
-	'ngInject';
+class BlockChainController{
+	constructor() {
+		'ngInject';
+
+		this.expanded = false;
+		this.currentBlocks = [];
+		this.totalBlocks = 20;
+		this.startFrom = 15;
+		this.populateBlocks();
+	}
+	
+	populateBlocks()
+	{
+		for(let i=this.currentBlocks.length; i < this.totalBlocks; ++i)
+		{
+			this.currentBlocks.push({
+				number: i+1,
+			});
+		}
+	}
+
+	toggle()
+	{
+		this.expanded = !this.expanded;
+	}
+
+	fetchInfo(number)
+	{
+		
+	}
 
 }
 
