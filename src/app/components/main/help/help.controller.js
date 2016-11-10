fa539349edf57ebc12ce7b2dfd2d709e53e78ba6
shastri9999 <shastri9999@gmail.com>
2016-11-10 05:32:41 +0530

    immediate fix

--- src/app/components/main/help/help.controller.js
+++ src/app/components/main/help/help.controller.js
@@ -2,24 +2,31 @@
 
 function HelpController($http) {
 	'ngInject';
-    $http({
-    	method: 'GET',
-    	url: '/kyck-rest/blockchain/getnric'
-    }).then((s)=>{
-    	const data = JSON.parse(s.data.data.message.result.message);
-    	this.hashes = Object.keys(data).map((key)=>{
-    		return {
-    			requestId: key,
-    			nricId: data[key],
-    			hash: ''
-    		}
-    	});
-    })
+	$http({
+		method: 'GET',
+		url: '/kyck-rest/blockchain/getnric'
+	}).then((s)=>{
+		const data = JSON.parse(s.data.data.message.result.message);
+		this.hashes = Object.keys(data).map((key)=>{
+			return {
+				requestId: key,
+				nricId: data[key],
+				hash: ''
+			}
+		});
+	})
 
-    this.validate = function(hash)
-    {
-    	console.log(hash);
-    }
+	this.validate = function(hash)
+	{
+		console.log(hash);
+		// $http({
+		// 	method: 'POST',
+		// 	url: '/kyck-rest/blockchain/getnric',
+		// 	data: {}
+		// }).then((s)=>{
+
+		// })
+	}
 }
 
 export default HelpController;
