2a78f40d4d101e5db05f98dd953c0bceb1a1fb61
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2016-11-17 03:17:19 +0530

    calendar ui popup

--- src/app/components/main/calendar/calendar.controller.js
+++ src/app/components/main/calendar/calendar.controller.js
@@ -1,60 +1,72 @@
 'use strict';
 
+import dialogTemplateURL from './dialog.html';
+
 function CalendarController($scope, $mdDialog, $filter, AuthenticationService, CalendarService, moment, $rootScope) {
-	'ngInject';
+    'ngInject';
 
-	$scope.events = [];
+    $scope.events = [];
 
-  $rootScope.loadingProgress = false;
-  
-  const userId = AuthenticationService.getLoggedInUser().userId;
-  const isBroker = AuthenticationService.isBroker();
-  const month = 11;
-  if (!isBroker)
-  {
-    CalendarService.fetchMeetings(userId, month).then((data)=>{
-      console.log(data);
-      $scope.events = data.map((slot)=>{
+    $rootScope.loadingProgress = false;
+
+    const userId = AuthenticationService.getLoggedInUser().userId;
+    const isBroker = AuthenticationService.isBroker();
+    const month = 11;
+
+    function formatSlot(slot) {
+        var st = moment(slot.startTime, 'DD/MM/YYYY hh:mm').toDate();
+        var title = $filter('date')(st, "HH:mm a");
         return {
-          title: slot.meetingSubject,
-          start: moment(slot.startTime, 'DD/MM/YYYY hh:mm').toDate(),
-          end: moment(slot.endTime, 'DD/MM/YYYY hh:mm').toDate(),
-          ...slot
-        }      
-      });
-    });
-  }
-  else
-  {
-    CalendarService.fetchBrokerMeetings().then((data)=>{
-      $scope.events = data.map((slot)=>{
-        return {
-          title: slot.meetingSubject,
-          start: moment(slot.startTime, 'DD/MM/YYYY hh:mm').toDate(),
-          end: moment(slot.endTime, 'DD/MM/YYYY hh:mm').toDate(),
-          ...slot
+            title: title + " " + slot.meetingSubject,
+            start: moment(slot.startTime, 'DD/MM/YYYY hh:mm').toDate(),
+            end: moment(slot.endTime, 'DD/MM/YYYY hh:mm').toDate(),
+            customClass: slot.status,
+            ...slot
         }
-      });
-    });
-  }
-  $scope.eventClicked = function ($selectedEvent) {
-   let closePopup = function() {
-      alert = undefined;
-   }
-   const slot = $selectedEvent;
-   alert = $mdDialog.alert({
-    title: slot.meetingSubject,
-    textContent: slot.meetingContent,
-    ok: 'Close'
-   });
+    }
 
-    $mdDialog
-    .show(alert)
-    .finally(closePopup);
-  }
-  $scope.eventCreate = function ($date) {
+    if (!isBroker) {
+        CalendarService.fetchMeetings(userId, month).then((data) => {
+            console.log(data);
+            $scope.events = data.map(formatSlot);
+        });
+    } else {
+        CalendarService.fetchBrokerMeetings().then((data) => {
+            console.log(data);
+            $scope.events = data.map(formatSlot);
+        });
+    }
 
-  }
+    $scope.eventClicked = function($selectedEvent) {
+        let closePopup = function() {
+            alert = undefined;
+        }
+
+        const slot = $selectedEvent;
+
+        // alert = $mdDialog.alert({
+        //   title: slot.meetingSubject,
+        //   textContent: slot.meetingContent,
+        //   ok: 'Close'
+        // });
+
+        $mdDialog.show({
+            parent: angular.element(document.body),
+            templateUrl: dialogTemplateURL,
+            controller: DialogController
+        });
+
+        function DialogController($scope, $mdDialog) {
+            'ngInject';
+            $scope.slot = slot;
+
+            $scope.closeDialog = function() {
+                $mdDialog.hide();
+            }
+        }
+    }
+
+    $scope.eventCreate = function($date) {}
 }
 
 export default CalendarController;
