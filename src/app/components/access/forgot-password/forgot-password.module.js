128db1d04378882c54ca1a083453a4f1292c0c68
shastri9999 <shastri9999@gmail.com>
2016-11-25 11:18:29 +0530

    Forgot password fixes

--- src/app/components/access/forgot-password/forgot-password.module.js
+++ src/app/components/access/forgot-password/forgot-password.module.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import route from './forgot-password.route';
+import './forgot-password.scss';
 
 const ForgotPasswordModule = angular.module('forgot-password-module', [
 	'ui.router'
