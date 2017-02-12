57afe5c76c73a56dbbe574ffbca5841b9cfc7cd3
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2017-02-12 15:34:10 +0530

    a

--- src/app/components/main/calendar/calendar.controller.js
+++ src/app/components/main/calendar/calendar.controller.js
@@ -1,11 +1,9 @@
 'use strict';
-
 import dialogTemplateURL from './dialog.html';
 import calSelectDialogURL from './calselect.html';
 
 function CalendarController($scope, $mdDialog, $filter, AuthenticationService, BrokerageResource, CalendarService, moment, $rootScope, $window, $mdToast) {
     'ngInject';
-
     $scope.events = [];
     $rootScope.loadingProgress = false;
     const userId = AuthenticationService.getLoggedInUser().userId;
@@ -14,34 +12,31 @@ function CalendarController($scope, $mdDialog, $filter, AuthenticationService, B
     fetchMeetings();
     setEventClick();
 
-    function getStatus (status, isBroker) {
-        var customClass="GREEN";
-        var ifConfirmButton=true;
-        var ifRescheduleButton=true;
-        var ifJoinVideoButton=true;
-        var formattedStatus=status;
-        var extraContent="";
-
-        if (status=="CONFIRM") {
-            formattedStatus="Confirmed";
-
-            ifConfirmButton=false;
+    function getStatus(status, isBroker) {
+        var customClass = "GREEN";
+        var ifConfirmButton = true;
+        var ifRescheduleButton = true;
+        var ifJoinVideoButton = true;
+        var formattedStatus = status;
+        var extraContent = "";
+        if (status == "CONFIRM") {
+            formattedStatus = "Confirmed";
+            ifConfirmButton = false;
         }
         if (status == "PENDING") {
-            formattedStatus="Pending";
-            customClass="GOLDEN";
-            ifConfirmButton=false;
-            ifJoinVideoButton=false;
+            formattedStatus = "Pending";
+            customClass = "GOLDEN";
+            ifConfirmButton = false;
+            ifJoinVideoButton = false;
         }
         else if (status == "REJECT") {
-            formattedStatus="Rejected"
-            customClass="RED";
-
-            ifJoinVideoButton=false;
-            ifRescheduleButton=false;
-            ifConfirmButton=false;
+            formattedStatus = "Rejected"
+            customClass = "RED";
+            ifJoinVideoButton = false;
+            ifRescheduleButton = false;
+            ifConfirmButton = false;
         }
-        else if(status == "NEW" || status == "RESCHEDULE") {
+        else if (status == "NEW" || status == "RESCHEDULE") {
             customClass = "GOLDEN";
             formattedStatus = "Pending";
             ifJoinVideoButton = false;
@@ -74,18 +69,17 @@ function CalendarController($scope, $mdDialog, $filter, AuthenticationService, B
         var st = moment(slot.startTime, 'DD/MM/YYYY hh:mm').toDate();
         var title = $filter('date')(st, "HH:mm a");
         var vals = getStatus(slot.status, isBroker);
-
         return {
-            title: slot.meetingSubject,
-            start: moment(slot.startTime, 'DD/MM/YYYY hh:mm').toDate(),
-            end: moment(slot.endTime, 'DD/MM/YYYY hh:mm').toDate(),
-            customClass: vals[0],
-            ifConfirmButton: vals[1],
-            ifRescheduleButton: vals[2],
-            ifJoinVideoButton: vals[3],
-            formattedStatus: vals[4],
-            extraContent: vals[5],
-            ...slot
+            title: slot.meetingSubject
+            , start: moment(slot.startTime, 'DD/MM/YYYY hh:mm').toDate()
+            , end: moment(slot.endTime, 'DD/MM/YYYY hh:mm').toDate()
+            , customClass: vals[0]
+            , ifConfirmButton: vals[1]
+            , ifRescheduleButton: vals[2]
+            , ifJoinVideoButton: vals[3]
+            , formattedStatus: vals[4]
+            , extraContent: vals[5]
+            , ...slot
         }
     }
 
@@ -93,9 +87,11 @@ function CalendarController($scope, $mdDialog, $filter, AuthenticationService, B
         $scope.events = [];
         if (!isBroker) {
             CalendarService.fetchMeetings().then((data) => {
+                console.log(data);
                 $scope.events = data.map(formatSlot);
             });
-        } else {
+        }
+        else {
             CalendarService.fetchBrokerMeetings().then((data) => {
                 // var eles = document.getElementsByClassName('md-event-calendar-month-cell');
                 // console.log(eles);
@@ -103,25 +99,21 @@ function CalendarController($scope, $mdDialog, $filter, AuthenticationService, B
                 //     console.log(n, (($window.innerHeight - 250) / 5) + "px");
                 //     eles[n].style.maxHeight = (($window.innerHeight - 200) / 5) + "px";
                 // }
-
                 $scope.events = data.map(formatSlot);
             });
         }
     }
 
-    function setEventClick () {
-        $scope.eventClicked = function($selectedEvent) {
-
-            let closePopup = function() {
+    function setEventClick() {
+        $scope.eventClicked = function ($selectedEvent) {
+            let closePopup = function () {
                 alert = undefined;
             }
-
             var slot = $selectedEvent;
-
             $mdDialog.show({
-                parent: angular.element(document.body),
-                templateUrl: dialogTemplateURL,
-                controller: DialogController
+                parent: angular.element(document.body)
+                , templateUrl: dialogTemplateURL
+                , controller: DialogController
             });
 
             function DialogController($scope, $mdDialog) {
@@ -129,76 +121,68 @@ function CalendarController($scope, $mdDialog, $filter, AuthenticationService, B
                 $scope.slot = slot;
                 $scope.isBroker = isBroker;
                 $scope.updateAppointment = updateAppointment;
-
-                $scope.closeDialog = function() {
+                $scope.closeDialog = function () {
                     $mdDialog.hide();
                 }
-
                 var parentEl = angular.element(document.body);
 
                 function updateAppointment(status) {
                     if (status == "JOIN") {
-                        BrokerageResource.getroom({},{
-                            'calendarId':$scope.slot.calendarId, 
-                            'userId' : $scope.isBroker ? $scope.slot.userEmailId : $scope.slot.brokerEmailId
-                        }, function(req){
+                        BrokerageResource.getroom({}, {
+                            'calendarId': $scope.slot.calendarId
+                            , 'userId': $scope.isBroker ? $scope.slot.userEmailId : $scope.slot.brokerEmailId
+                        }, function (req) {
                             $rootScope.loadingProgress = false;
                             $window.open(req.data, 'Join Video Conferenence', 'width=1024,height=800');
                             $mdToast.showSimple("Invited for Video Conference.")
-                        }, function(error){console.log(error);});
-                        $mdToast.showSimple("Invited for Video Conference."); 
+                        }, function (error) {
+                            console.log(error);
+                        });
+                        $mdToast.showSimple("Invited for Video Conference.");
                     }
                     else if (status == "RESCHEDULE") {
                         var partner = {
-                            'brokerageName' : slot['brokerName'],
-                            'img' : '/assets/images/partnerLogos/' + $scope.slot['brokerName'] + '.png',
-                            'showCalendar' : true,
-                            'selectedAppointments' : []
+                            'brokerageName': slot['brokerName']
+                            , 'img': '/assets/images/partnerLogos/' + $scope.slot['brokerName'] + '.png'
+                            , 'showCalendar': true
+                            , 'selectedAppointments': []
                         };
-
                         var selectedPartners = [partner];
-                        
                         $mdDialog.show({
-                            parent: parentEl,
-                            templateUrl: calSelectDialogURL,
-                            controller: DialogController2
+                            parent: parentEl
+                            , templateUrl: calSelectDialogURL
+                            , controller: DialogController2
                         });
 
                         function DialogController2($scope, $mdDialog) {
                             'ngInject';
-
                             $scope.selectedPartners = selectedPartners;
                             $scope.timeslotSelected = true;
-                            
-                            $rootScope.$on('timeslotSelected', function(event, data) {
+                            $rootScope.$on('timeslotSelected', function (event, data) {
                                 var calendarDetailRequest = {
-                                  "calenderId": slot.calendarId,
-                                  "calenderSlot": data[0].selectedAppointments[0],
-                                  "meetingContent": slot.meetingContent,
-                                  "meetingLocation": slot.meetingLocation,
-                                  "meetingStatus": isBroker ? "NEW" : "PENDING",
-                                  "meetingSubject": slot.meetingSubject
+                                    "calenderId": slot.calendarId
+                                    , "calenderSlot": data[0].selectedAppointments[0]
+                                    , "meetingContent": slot.meetingContent
+                                    , "meetingLocation": slot.meetingLocation
+                                    , "meetingStatus": isBroker ? "NEW" : "PENDING"
+                                    , "meetingSubject": slot.meetingSubject
                                 }
-
                                 CalendarService.updateAppointmentEvent(calendarDetailRequest).then((data) => {
                                     $mdToast.showSimple("Meeting has been rescheduled.");
                                     fetchMeetings();
                                 });
                             });
-                    
-                            $scope.closeDialog = function() {
+                            $scope.closeDialog = function () {
                                 $mdDialog.hide();
                             }
                         }
                     }
                     else {
                         var calendarDetailRequest = {
-                          "calendarId": slot.calendarId,
-                          "meetingStatus": status
+                            "calendarId": slot.calendarId
+                            , "meetingStatus": status
                         };
-
                         console.log(slot, calendarDetailRequest);
-
                         CalendarService.updateAppointmentStatus(calendarDetailRequest).then((data) => {
                             console.log(slot, calendarDetailRequest, data);
                             $mdToast.showSimple("Status of the meeting updated.");
@@ -210,8 +194,6 @@ function CalendarController($scope, $mdDialog, $filter, AuthenticationService, B
             }
         }
     }
-
-    $scope.eventCreate = function($date) {}
+    $scope.eventCreate = function ($date) {}
 }
-
 export default CalendarController;
